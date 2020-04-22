d7cef979383358e3f51eca28b2d1808d82bc8219
jimholman1199 <jimholman1199@gmail.com>
2020-04-22 20:20:20 +0300

    Updates

---
+++ precache-manifest.a668019897f93dfacae7e7845ab431d3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82cd26733e2367f6c811b33e99c7eba",
+    "url": "/reactCV/index.html"
+  },
+  {
+    "revision": "2c59caa6b7a7f2be6a81",
+    "url": "/reactCV/static/css/main.a7f79e8f.chunk.css"
+  },
+  {
+    "revision": "b8cbe12211ae1e1fc2bb",
+    "url": "/reactCV/static/js/2.7f2b7997.chunk.js"
+  },
+  {
+    "revision": "2c59caa6b7a7f2be6a81",
+    "url": "/reactCV/static/js/main.dc40dd43.chunk.js"
+  },
+  {
+    "revision": "4225dabe3f7616710611",
+    "url": "/reactCV/static/js/runtime-main.9c42ad27.js"
+  },
+  {
+    "revision": "1d0e244f9d9ceb9ff2b351cd0dea8c37",
+    "url": "/reactCV/static/media/big-head.1d0e244f.png"
+  },
+  {
+    "revision": "c8c703bcfcb5980f9be44ae310f2a5f5",
+    "url": "/reactCV/static/media/carpentry.c8c703bc.jpg"
+  },
+  {
+    "revision": "f2281d6f7a05e3d093b3b807f3e3595f",
+    "url": "/reactCV/static/media/dinesh.f2281d6f.png"
+  },
+  {
+    "revision": "b9a949e6d87ecf1e6f428f889844e31b",
+    "url": "/reactCV/static/media/games.b9a949e6.jpg"
+  },
+  {
+    "revision": "adc6ab69b4b57c603ce1ba13d4d2a9fe",
+    "url": "/reactCV/static/media/gavin.adc6ab69.png"
+  },
+  {
+    "revision": "2e274ee748b5a8ef1e5340c377aa9668",
+    "url": "/reactCV/static/media/gilfoyle.2e274ee7.png"
+  },
+  {
+    "revision": "64f71a9d959d7968a2a0e492d6998da1",
+    "url": "/reactCV/static/media/jared.64f71a9d.png"
+  },
+  {
+    "revision": "4b37a594718d1c7229e86a9646815ce8",
+    "url": "/reactCV/static/media/me.4b37a594.jpg"
+  },
+  {
+    "revision": "ea01bdce3d31c6cdb881c70a33f885a6",
+    "url": "/reactCV/static/media/monica.ea01bdce.png"
+  },
+  {
+    "revision": "1d6e2aa292dccfa6ee762a28468bad00",
+    "url": "/reactCV/static/media/music-davidGuetta.1d6e2aa2.jpg"
+  },
+  {
+    "revision": "8c9271c960fb4aaaae952df05b134a5d",
+    "url": "/reactCV/static/media/music.8c9271c9.jpg"
+  },
+  {
+    "revision": "ba5f5b370193726eb748d3b82bd4d51c",
+    "url": "/reactCV/static/media/programing.ba5f5b37.jpg"
+  },
+  {
+    "revision": "c6230b11b6611062a6d8246c39e3433e",
+    "url": "/reactCV/static/media/richard.c6230b11.png"
+  },
+  {
+    "revision": "b5c658e8cb72e193a3d24cd1cc668a3f",
+    "url": "/reactCV/static/media/travel-airplane.b5c658e8.jpg"
+  },
+  {
+    "revision": "2ccd604efd18a0958610e08368ea7b9d",
+    "url": "/reactCV/static/media/waterfall.2ccd604e.gif"
+  }
+]);
